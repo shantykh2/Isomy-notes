7ad00866353f90626d931d1df75d67d2cc4c83dd
shantykh2 <shanty.kh2@gmail.com>
2025-03-07 15:12:39 +0700

    Update

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ $result = $stmt->get_result();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>❀ Community Notes ❀</title>
+    <title>❀ Isomy Notes ❀</title>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
